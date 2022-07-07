d4c7e0749a6bd84ad50b30591e98f8d4afa5c631
Dominik Roszkowski <dominik@roszkowski.dev>
2022-07-07 22:22:20 +0200

    feat: add buttons to the weight selector

--- packages/app_ui/lib/src/widgets/number_selector.dart
+++ packages/app_ui/lib/src/widgets/number_selector.dart
@@ -1,5 +1,6 @@
 import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 
 class WeightSelectionLayout extends StatefulWidget {
@@ -7,10 +8,12 @@ class WeightSelectionLayout extends StatefulWidget {
     super.key,
     required this.initialWeight,
     required this.onChanged,
+    required this.onSubmitted,
   });
 
   final double initialWeight;
   final void Function(double) onChanged;
+  final void Function(double) onSubmitted;
 
   @override
   State<WeightSelectionLayout> createState() => _WeightSelectionLayoutState();
@@ -29,6 +32,7 @@ class _WeightSelectionLayoutState extends State<WeightSelectionLayout> {
   Widget build(BuildContext context) {
     final fractionalPart = widget.initialWeight.fractionalValue();
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
         const SizedBox(
           height: 10,
@@ -57,7 +61,7 @@ class _WeightSelectionLayoutState extends State<WeightSelectionLayout> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 24.0),
+                    padding: const EdgeInsets.only(bottom: 24),
                     child: SizedBox(
                       width: 3,
                       height: 120,
@@ -80,7 +84,44 @@ class _WeightSelectionLayoutState extends State<WeightSelectionLayout> {
               ),
             ),
           ),
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.errorContainer,
+                    elevation: 0,
+                  ),
+                  onPressed: () {
+                    Navigator.maybePop(context);
+                  },
+                  child: Text(
+                    MaterialLocalizations.of(context).cancelButtonLabel,
+                  ),
+                ),
+              ),
+              const Gap(8),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.secondary,
+                    elevation: 0,
+                  ),
+                  onPressed: () {
+                    widget.onSubmitted(currentValue);
+                    Navigator.maybePop(context);
+                  },
+                  child: Text(
+                    MaterialLocalizations.of(context).saveButtonLabel,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
